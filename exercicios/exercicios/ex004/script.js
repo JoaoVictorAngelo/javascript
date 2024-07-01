3d93edfd4b580a7cc0cb44a2f78a9ffab7fd52bb
Joao Victor Angelo <146788838+JoaoVictorAngelo@users.noreply.github.com>
2024-07-01 19:53:02 -0300

    feito exercício 4 (tabuada)

--- exercicios/exercicios/ex004/script.js
+++ exercicios/exercicios/ex004/script.js
@@ -1,1 +1,21 @@
-alert('Ola')
+function tabular() {
+    // pegando numero para tabular
+    var numeroEscolhido = Number(document.getElementById('numeroEscolhido').value)
+    var contagem = Number(0)
+    var res = Number()
+    var solucao = document.getElementById('solucao')
+
+        // Limpando o resultado anterior
+        solucao.innerHTML = ""
+
+
+    // fazendo tabuada
+    while (contagem <= 10){
+        var res = numeroEscolhido * contagem
+        var listItem = document.createElement('li')
+        listItem.textContent = `${numeroEscolhido} x ${contagem} = ${res}`
+        solucao.appendChild(listItem);
+        contagem ++
+    }
+    
+}
